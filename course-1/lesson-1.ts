f527cb2bf2cb77d915fac4a801dad696cc44df1b
nductoan <toanbeo090400@gmail.com>
2023-04-20 15:28:49 +0700

    lesson-1 setup

---
+++ course-1/lesson-1.ts
@@ -0,0 +1,6 @@
+//------------------ lesson-1 ----------------------
+var message: string = "Hello World TypeScript";
+console.log(message);
+var course: string =
+  "https://www.youtube.com/watch?v=lE4DfZKlwDA&list=PLncHg6Kn2JT5emvXmG6kgeGkrQjRqxsb4&index=1&t=443s";
+console.log(course);
