d6ffde64355618834250aaa2a7d5ee6233d84b58
nductoan <toanbeo090400@gmail.com>
2023-04-20 15:36:36 +0700

    lesson-8 switch-case

---
+++ course-1/lesson-8.ts
@@ -0,0 +1,16 @@
+//------------------ lesson-8 ----------------------
+const key: number = 30;
+switch (key) {
+  case 1:
+    console.log(">> key:", key);
+    break;
+  case 2:
+    console.log(">> key:", key);
+    break;
+  case 3:
+    console.log(">> key:", key);
+    break;
+  default:
+    console.log(">> default key:", key);
+    break;
+}
