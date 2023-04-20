512d4b11147e4a8d98d94caed821c7f23f415dba
nductoan <toanbeo090400@gmail.com>
2023-04-20 15:33:54 +0700

    lesson-5 explicit types any

---
+++ course-1/lesson-5.ts
@@ -0,0 +1,14 @@
+//------------------ lesson-5 ----------------------
+let age: any = 25;
+
+age = true;
+console.log(age);
+
+age = "hello";
+console.log(age);
+
+age = { name: "luigi" };
+console.log(age);
+
+let mixed: any[] = ["name", 10, { name: "name-001" }, true, [10, 20]];
+console.log(mixed);
