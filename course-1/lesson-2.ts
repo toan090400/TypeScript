e67b45bd9e855c7094f783efdddf1afa17098507
nductoan <toanbeo090400@gmail.com>
2023-04-20 15:30:31 +0700

    lesson-2

---
+++ course-1/lesson-2.ts
@@ -0,0 +1,20 @@
+//------------------ lesson-2 ----------------------
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+
+// character = 20;
+character = "luigi";
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+// console.log(area('hello'));
+console.log(circ(7.5));
